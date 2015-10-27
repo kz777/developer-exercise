6a271153f180f2a226c71db357ad59f85e9aa3c9
ASLAN USSAYEV <usprof777@gmail.com>
2015-10-27 15:37:35 -0500

    Describe Deck and Hand classes

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -99,22 +99,63 @@ describe Card do
 end
 
 
-# class DeckTest < Test::Unit::TestCase
-#   def setup
-#     @deck = Deck.new
-#   end
+describe Deck do
 
-#   def test_new_deck_has_52_playable_cards
-#     assert_equal @deck.playable_cards.size, 52
-#   end
+  it "should build 52 cards" do
+    Deck.build_cards.length.should eq(52)
+  end
 
-#   def test_dealt_card_should_not_be_included_in_playable_cards
-#     card = @deck.deal_card
-#     assert(@deck.playable_cards.include?(card))
-#   end
+  it "should have 52 cards when new deck" do
+    Deck.new.cards.length.should eq(52)
+  end
 
-#   def test_shuffled_deck_has_52_playable_cards
-#     @deck.shuffle
-#     assert_equal @deck.playable_cards.size, 52
-#   end
-# end
+end
+
+
+describe Hand do
+
+  it "should calculate the value correctly" do
+    deck = double(:deck, :cards => [Card.new(:clubs, 4), Card.new(:diamonds, 10)])
+    hand = Hand.new
+    2.times { hand.hit!(deck) }
+    expect(hand.value).to eq(14)
+  end
+
+  it "should take from the top of the deck" do
+    club4 = Card.new(:clubs, 4)
+    diamond7 = Card.new(:diamonds, 7) 
+    clubK = Card.new(:clubs, "K")
+
+    deck = double(:deck, :cards => [club4, diamond7, clubK])
+    hand = Hand.new
+    2.times { hand.hit!(deck) }
+    expect(hand.cards).to eq([club4, diamond7])
+
+  end
+
+  describe "#play_as_dealer" do
+    it "should hit below 17" do
+      deck = double(:deck, :cards => [Card.new(:clubs, 4), Card.new(:diamonds, 4), Card.new(:clubs, 2), Card.new(:hearts, 7)])
+      hand = Hand.new
+      2.times { hand.hit!(deck) }
+      hand.play_as_dealer(deck)
+      expect(hand.value).to eq(17)
+    end
+    it "should not hit above" do
+      deck = double(:deck, :cards => [Card.new(:clubs, 8), Card.new(:diamonds, "Q")])
+      hand = Hand.new
+      2.times { hand.hit!(deck) }
+      hand.play_as_dealer(deck)
+      expect(hand.value).to eq(18)
+    end
+    it "should stop on 21" do
+      deck = double(:deck, :cards => [Card.new(:clubs, 4), 
+                                    Card.new(:diamonds, 7), 
+                                    Card.new(:clubs, "K")])
+      hand = Hand.new
+      2.times { hand.hit!(deck) }
+      hand.play_as_dealer(deck)
+      expect(hand.value).to eq(21)
+    end
+  end
+end
