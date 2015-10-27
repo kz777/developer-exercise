4880485288877f12e20f727a7d77cba38f040133
ASLAN USSAYEV <usprof777@gmail.com>
2015-10-27 15:58:59 -0500

    Describe a Game class

--- ruby/blackjack/blackjack.rb
+++ ruby/blackjack/blackjack.rb
@@ -158,3 +158,43 @@ describe Hand do
     end
   end
 end
+
+
+describe Game do
+
+  it "should have a players hand" do
+    expect(Game.new.player_hand.cards.length).to eq(2)
+  end
+  it "should have a dealers hand" do
+    expect(Game.new.dealer_hand.cards.length).to eq(2)
+  end
+  it "should have a status" do
+    expect(Game.new.status).to_not be_nil
+  end
+  it "should hit when I tell it to" do
+    game = Game.new
+    game.hit
+    expect(game.player_hand.cards.length).to eq(3)
+  end
+
+  it "should play the dealer hand when I stand" do
+    game = Game.new
+    game.stand
+    expect(game.status[:winner]).to_not be_nil
+  end
+
+  describe "#determine_winner" do
+    it "should have dealer win when player busts" do
+      expect(Game.new.determine_winner(22, 15)).to eq(:dealer) 
+    end
+    it "should player win if dealer busts" do
+      expect(Game.new.determine_winner(18, 22)).to eq(:player) 
+    end
+    it "should have player win if player > dealer" do
+      expect(Game.new.determine_winner(18, 16)).to eq(:player) 
+    end
+    it "should have push if tie" do
+      expect(Game.new.determine_winner(16, 16)).to eq(:push) 
+    end
+  end
+end
